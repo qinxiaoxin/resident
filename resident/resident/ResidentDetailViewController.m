a56b9d952ccaf6fe5dd16c0fbc4f75e77c66ca13
秦 鑫 <qinxin5986461@gmail.com>
2015-02-06 22:42:20 +0800

    DetailViewController

--- resident/resident/ResidentDetailViewController.m
+++ resident/resident/ResidentDetailViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "ResidentDetailViewController.h"
+#import "UIScrollView+TwitterCover.h"
 
 @interface ResidentDetailViewController ()
 
-@property (weak, nonatomic) IBOutlet UILabel *originLabel;
-@property (weak, nonatomic) IBOutlet UILabel *transplantLabel;
-@property (weak, nonatomic) IBOutlet UIImageView *residentCover;
+@property (nonatomic, strong) UIScrollView *scrollView;
+
+@property (nonatomic, strong) UILabel *briefLabel;
+@property (nonatomic, strong) UILabel *historyLabel;
+@property (nonatomic, strong) UILabel *storyLabel;
 
 
 @end
@@ -33,6 +36,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)dealloc
+{
+    [_scrollView removeTwitterCoverView];
+}
+
 
 #pragma mark - Init
 
@@ -45,9 +53,60 @@
 
 - (void)initView
 {
-    _originLabel.text = [self.residentDic valueForKeyPath:@"origin"];
-    _transplantLabel.text = [self.residentDic valueForKeyPath:@"transplant"];
-    _residentCover.image = [UIImage imageNamed:[self.residentDic valueForKeyPath:@"image"]];
+    
+    if ([self respondsToSelector:@selector(setEdgesForExtendedLayout:)]) {
+        [self setEdgesForExtendedLayout:UIRectEdgeNone];
+    }
+    
+    
+    _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    NSString *imageStr = [self.residentDic valueForKeyPath:@"image"];
+    if (imageStr) {
+        [_scrollView addTwitterCoverWithImage:[UIImage imageNamed:imageStr]];
+    }
+    [self.view addSubview:_scrollView];
+    
+    
+    _briefLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    _briefLabel.numberOfLines = 0;
+    _briefLabel.font = [UIFont systemFontOfSize:16];
+    NSString *briefText = [[self.residentDic valueForKeyPath:@"brief"] stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"];
+    _briefLabel.text = briefText;
+    NSDictionary *briefAttribute = @{NSFontAttributeName: [UIFont systemFontOfSize:16]};
+    CGSize briefLableSize = [_briefLabel.text boundingRectWithSize:CGSizeMake(SCREEN_WIDTH - 40, 0) options: NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:briefAttribute context:nil].size;
+    _briefLabel.frame = CGRectMake(20, SCREEN_WIDTH + 100, briefLableSize.width, briefLableSize.height);
+    [_scrollView addSubview:_briefLabel];
+    
+    
+    
+    
+    
+    _historyLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    _historyLabel.numberOfLines = 0;
+    _historyLabel.font = [UIFont systemFontOfSize:16];
+    NSString *historyText = [[self.residentDic valueForKeyPath:@"history"] stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"];
+    _historyLabel.text = historyText;
+    NSDictionary *historyAttribute = @{NSFontAttributeName: [UIFont systemFontOfSize:16]};
+    CGSize historyLabelSize = [_historyLabel.text boundingRectWithSize:CGSizeMake(SCREEN_WIDTH - 40, 0) options: NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:historyAttribute context:nil].size;
+    _historyLabel.frame = CGRectMake(20, _briefLabel.bottom + 100, historyLabelSize.width, historyLabelSize.height);
+    [_scrollView addSubview:_historyLabel];
+    
+    
+    
+    
+    
+    _storyLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    _storyLabel.numberOfLines = 0;
+    _storyLabel.font = [UIFont systemFontOfSize:16];
+    NSString *storyText = [[self.residentDic valueForKeyPath:@"story"] stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"];
+    _storyLabel.text = storyText;
+    NSDictionary *storyAttribute = @{NSFontAttributeName: [UIFont systemFontOfSize:16]};
+    CGSize storyLabelSize = [_storyLabel.text boundingRectWithSize:CGSizeMake(SCREEN_WIDTH - 40, 0) options: NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:storyAttribute context:nil].size;
+    _storyLabel.frame = CGRectMake(20, _historyLabel.bottom + 100, storyLabelSize.width, storyLabelSize.height);
+    [_scrollView addSubview:_storyLabel];
+    
+    _scrollView.contentSize = CGSizeMake(SCREEN_WIDTH, _storyLabel.bottom + 100);
+    
 }
 
 /*
